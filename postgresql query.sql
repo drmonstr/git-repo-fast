97094bc443c72f841b7b92a3b344712636794490
drmonstr <dr.monstr@gmail.com>
2017-07-20 16:32:23 +0300

    Add files via upload
    Запрос, который выберет имена клиентов, которые когда-либо покупали молоко, но не покупали сметану за последний месяц.

---
+++ postgresql query.sql
@@ -0,0 +1,5 @@
+﻿select clients.name from clients
+where exists(select * from sales where clients.id = sales.client_id and sales.name = 'milk')
+except
+select clients.name from clients
+where exists(select * from sales where clients.id = sales.client_id and sales.name = 'smetana' and date'today' - sales.date < interval '1 month')
